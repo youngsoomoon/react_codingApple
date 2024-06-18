456b44a337bbaf57d0daa238e8ef81f29b5cb844
문영수 <moonyoungsu@naver.com>
2024-06-18 21:51:13 +0900

    commit input 1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
     let [title, modifyTitle] = useState(["남자코트 추천", "강남우동맛집", "JS독학"]);
     let [thumbsUp, c] = useState([0, 0, 0]);
     let [modal, setModal] = useState(false);
-    let [newTitle, setNewTitle] = useState([0, 1, 2]);
+    let [index, setIndex] = useState(0);
+    let [inputValue, setInputValue] = useState('');
 
     return (
         <div className="App">
@@ -37,12 +38,13 @@ function App() {
                     return (
                         <div className="list" key={i}>
                             <h4 onClick={() => {
-                                setModal(true);
-                                //TODO: setNewTitle 구현
-                                setNewTitle(i);
+                                setModal(!modal);
+                                setIndex(i);
                             }}>
                                 {title[i]}
-                                <span onClick={() => {
+                                <span onClick={(e) => {
+                                    //이벤트 버블링을 막기위한 stopPropagation
+                                    e.stopPropagation();
                                     let copy = [...thumbsUp];
                                     copy[i] = copy[i] + 1;
                                     c(copy);
@@ -54,8 +56,12 @@ function App() {
                 })
             }
 
+            {/* 숙제: 1. 글 추가 버튼( 입력값 타이틀 리스트에 추가) */}
+            {/* 숙제: 2. 글 삭제 버튼( 각 리스트에 버튼으로 존재) */}
+            <input onChange={(e) => { setInputValue(e.target.value); console.log(inputValue); }}></input>
+
             {
-                modal === true ? <Modal title={title} modifyTitle={modifyTitle} setNewTitle={newTitle} /> : null
+                modal === true ? <Modal title={title} modifyTitle={modifyTitle} index={index} /> : null
             }
         </div>
     );
@@ -64,7 +70,7 @@ function App() {
 function Modal(props) {
     return (
         <div className="modal">
-            <h4>{props.title[props.newTitle]}</h4>
+            <h4>{props.title[props.index]}</h4>
             <p>날짜</p>
             <p>상세내용</p>
             <button onClick={() => {
